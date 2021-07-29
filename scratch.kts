8972a1deba4d9d1a5b24415eb7a8be2cca5908c5
thuankhang <51144812+thuankhang@users.noreply.github.com>
2021-07-29 20:24:44 +0700

    Homework

---
+++ scratch.kts
@@ -0,0 +1,10 @@
+
+val Pi: Double = 3.14
+val Year: Int = 2005
+val Age: Byte = 16
+val School: String = "THPT Trung Văn"
+println("Số Pi là: $Pi")
+println("Năm sinh: $Year")
+println("Tuổi: $Age")
+println("Trường: $School")
+
---
+++ scratch_1.kts
@@ -0,0 +1,13 @@
+val a: Int = 5
+val b: Int = 9
+val S = a * b
+println("Diện tích hcn có 2 cạnh lần lượt 5 và 9 là: $S")
+
+val Pi = 3.14
+val R = 2.4
+val p = 2 * Pi * R
+println("Chu vi hình tròn bán kính R = 2.4 là: $p ")
+
+val c: Int = 2
+val h = Math.sqrt(c*c*c*c.toDouble())
+println("Cạnh huyền tam giác vuông cân cạnh 2 là: $h")
