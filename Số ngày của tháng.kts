c235b9c72644124b5f696091fbcc6e0c873c0ce1
thuankhang <51144812+thuankhang@users.noreply.github.com>
2021-08-07 10:24:03 +0700

    6/8

--- Số ngày của tháng.kts
+++ Số ngày của tháng.kts
@@ -8,32 +8,29 @@ print("Write month: ")
 month = readLine()!!.toInt()
 print("Write year: ")
 year = readLine()!!.toInt()
-if (month != 2) {
-    if (month < 8){
-        m = month % 2
-        if (m == 1){
-            print("31 days")
+if (month in 1..12) {
+    if (month != 2) {
+        if (month < 8) {
+            m = month % 2
+            if (m == 1) {
+                print("31 days")
+            } else print("30 days")
+        } else {
+            m = month % 2
+            if (m == 1) {
+                print("30 days")
+            } else print("31 days")
         }
-        else print("30 days")
-    }
-    else{
-        m = month % 2
-        if (m == 1){
-            print("30 days")
-        }
-        else print("31 days")
+    } else {
+        y = year % 4
+        y1 = year % 100
+        y2 = year % 400
+        if (y == 0) {
+            if (y1 == 0) {
+                if (y2 == 0) print("29 days")
+                else print("28 days")
+            } else print("29 days")
+        } else print("28 days")
     }
 }
-else{
-    y = year % 4
-    y1 = year % 100
-    y2 = year % 400
-    if (y == 0 ){
-        if (y1 == 0){
-            if (y2 == 0) print("29 days")
-            else print("28 days")
-        }
-        else print("29 days")
-    }
-    else print("28 days")
-}
+else print("Error")
