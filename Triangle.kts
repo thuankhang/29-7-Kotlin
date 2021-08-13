caef6b879a628e7d756534e03cba2dc7a87a0e4e
thuankhang <matthuanphong@gmail.com>
2021-08-13 14:34:39 +0700

    Friday August 13th

--- Triangle.kts
+++ Triangle.kts
@@ -3,7 +3,7 @@ val point = "*"
 val space = " "
 val numberSpaceOut = arrayOf(2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50)
 val numberSpaceIn = arrayOf(1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49)
-var numOut = 1
+var numIn = 1
 print("Write side length: ")
 var length = readLine()!!.toInt()
 if (length >= 3) {
@@ -13,12 +13,12 @@ if (length >= 3) {
     do {
         length -= 1
         numSpaceOut = numberSpaceOut[length]
-        val numSpaceIn = numberSpaceIn[numOut]
+        val numSpaceIn = numberSpaceIn[numIn]
         println(space.repeat(numSpaceOut) + point + space.repeat(numSpaceIn) + point)
-        numOut += 2
+        numIn += 2
     }
     while (length != 0)
-    side = (point + space).repeat(numOut + 2)
+    side = (point + space).repeat(numIn + 2)
     print(side)
 }
 
